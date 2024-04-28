577f1ab0ba74849a7c5676b4a288730adc9a10fc
Oumb2021 <baarryoumar@gmail.com>
2024-04-28 00:12:48 -0400

    Black and white theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <p>Only authenticated users can see this</p>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
