99018d74f38918aaee54771b3da9f9dd5d93bc1e
Oumb2021 <baarryoumar@gmail.com>
2024-05-01 02:37:32 -0400

    sidebar part 2

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/user-sidebar";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+import { Hint } from "@/components/hint";
 
 export const Toggle = () => {
   const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
@@ -12,21 +13,25 @@ export const Toggle = () => {
     <>
       {collapsed && (
         <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4">
-          <Button variant="ghost" className="h-auto p-2" onClick={onExpand}>
-            <ArrowRightFromLine className="h-4 w-4" />
-          </Button>
+          <Hint label={label} side="right" asChild>
+            <Button variant="ghost" className="h-auto p-2" onClick={onExpand}>
+              <ArrowRightFromLine className="h-4 w-4" />
+            </Button>
+          </Hint>
         </div>
       )}
       {!collapsed && (
         <div className="p-3 pl-6 mb-2 flex items-center w-full">
           <p className="font-semibold text-primary">For you</p>
-          <Button
-            className="h-auto p-2 ml-auto"
-            variant="ghost"
-            onClick={onCollapse}
-          >
-            <ArrowLeftFromLine className="h-4 w-4" />
-          </Button>
+          <Hint label={label} side="right" asChild>
+            <Button
+              className="h-auto p-2 ml-auto"
+              variant="ghost"
+              onClick={onCollapse}
+            >
+              <ArrowLeftFromLine className="h-4 w-4" />
+            </Button>
+          </Hint>
         </div>
       )}
     </>
