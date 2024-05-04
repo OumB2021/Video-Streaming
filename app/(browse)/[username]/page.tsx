4f826452425f0253ce6f96b43930512dbac4c318
John Royal <34844819+john-royal@users.noreply.github.com>
2024-05-04 01:39:03 -0400

    perf: optimize some db queries

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,27 +1,26 @@
-import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
 import { Actions } from "./_components/actions";
 
-interface UserPagePropos {
+interface UserPageProps {
   params: {
     username: string;
   };
 }
 
-const UserPage = async ({ params }: UserPagePropos) => {
+const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
+
   if (!user) {
     notFound();
   }
 
-  const isFollowing = await isFollowingUser(user.id);
   return (
     <div className="flex flex-col g-y-4">
       <p>username: {user.username}</p>
       <p>user id : {user.id}</p>
-      <p>is following: {`${isFollowing}`}</p>
-      <Actions userId={user.id} isFollowing={isFollowing} />
+      <p>is following: {`${user.isFollowing}`}</p>
+      <Actions userId={user.id} isFollowing={user.isFollowing} />
     </div>
   );
 };
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -1,9 +1,21 @@
+import { getSelf } from "./auth-service";
 import { db } from "./db";
 
 export const getUserByUsername = async (username: string) => {
+  const self = await getSelf().catch(() => null);
+
   const user = await db.user.findUnique({
     where: { username },
+    include: {
+      followedBy: self ? { where: { followerId: self.id } } : undefined,
+    },
   });
 
-  return user;
+  if (!user) return null;
+
+  const { followedBy, ...rest } = user;
+  return {
+    ...rest,
+    isFollowing: followedBy ? followedBy.length > 0 : false,
+  };
 };
