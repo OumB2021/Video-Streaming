f80634cafc1dd60098f9208a334a9d8777f8f545
Oumb2021 <baarryoumar@gmail.com>
2024-05-03 22:16:20 -0400

    connection modal

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -3,6 +3,7 @@ import { UrlCard } from "../chat/_components/url-card";
 import { getSelf } from "@/lib/auth-service";
 import { getStreamByUserId } from "@/lib/service-service";
 import { KeyCard } from "../chat/_components/key-card";
+import { ConnectModal } from "../chat/_components/connect-modal";
 
 const KeysPage = async () => {
   const self = await getSelf();
@@ -16,7 +17,7 @@ const KeysPage = async () => {
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl font-bold">Keys & URLs</h1>
-        <Button variant={"primary"}>Generate</Button>
+        <ConnectModal />
       </div>
       <div className="space-y-4">
         <UrlCard value={stream.serverUrl} />
