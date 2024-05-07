23c5a8f8eef2aa5af23db7510085c77f6fe9efe1
Oumb2021 <baarryoumar@gmail.com>
2024-05-06 22:21:53 -0400

    live video connection established

---
+++ components/stream-player/video.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  useConnectionState,
+  useRemoteParticipant,
+  useTracks,
+} from "@livekit/components-react";
+import { ConnectionState, Track } from "livekit-client";
+
+interface VideoProps {
+  hostName: string;
+  hostIdentity: string;
+}
+
+export const Video = ({ hostName, hostIdentity }: VideoProps) => {
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+  console.log(`what is participant: ${participant}`);
+
+  const tracks = useTracks([
+    Track.Source.Camera,
+    Track.Source.Microphone,
+  ]).filter((track) => track.participant.identity === hostIdentity);
+
+  let content;
+
+  if (!participant && connectionState === ConnectionState.Connected) {
+    content = <p>Host is offline</p>;
+  } else if (!participant || tracks.length === 0) {
+    content = <p>Loading...</p>;
+  } else {
+    content = <p>Live video</p>;
+  }
+  return <div className="aspect-video border-b group relative">{content}</div>;
+};
