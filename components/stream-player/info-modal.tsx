843aaf683115ef69a0b218a69515b0c8395c3e8c
Oumb2021 <baarryoumar@gmail.com>
2024-05-09 01:05:21 -0400

    edit card info bug fixed

--- components/stream-player/info-modal.tsx
+++ components/stream-player/info-modal.tsx
@@ -10,6 +10,11 @@ import {
 } from "../ui/dialog";
 
 import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { useState, useTransition, useRef, ElementRef } from "react";
+import { toast } from "sonner";
+import { updateStream } from "@/actions/stream";
 
 interface InfoModalProps {
   intialName: string;
@@ -17,6 +22,23 @@ interface InfoModalProps {
 }
 
 export const InfoModal = ({ intialName, initialThumbnail }: InfoModalProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+  const [name, setName] = useState(intialName);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startTransition(() => {
+      updateStream({ name: name })
+        .then(() => {
+          toast.success("Stream info updated");
+          closeRef?.current?.click();
+        })
+        .catch(() => toast.error("Stream info not updated"));
+    });
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -28,6 +50,27 @@ export const InfoModal = ({ intialName, initialThumbnail }: InfoModalProps) => {
         <DialogHeader>
           <DialogTitle>Edit stream info</DialogTitle>
         </DialogHeader>
+        <form onSubmit={onSubmit} className="space-y-14">
+          <div className="space-y-2">
+            <Label>Name</Label>
+            <Input
+              placeholder="Stream name"
+              onChange={onChange}
+              value={name}
+              disabled={isPending}
+            />
+          </div>
+          <div className="flex justify-between">
+            <DialogClose ref={closeRef} asChild>
+              <Button type="button" variant={"ghost"}>
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={isPending} type="submit" variant="primary">
+              Save
+            </Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
