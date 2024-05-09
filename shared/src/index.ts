9713b7ccd31acb2085bd6c54b207d6701a82a51d
John Royal <34844819+john-royal@users.noreply.github.com>
2024-05-09 15:30:19 -0400

    refactor: clean up shared types

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,22 +1,9 @@
-// Augment the socket.io and socket.io-client modules to include these as event types.
-
-// ListenEvents extends EventsMap = DefaultEventsMap, EmitEvents extends EventsMap = ListenEvents
-
-export type RTCOfferEvent = ["offer", RTCSessionDescriptionInit];
-
-export type RTCAnswerEvent = ["answer", RTCSessionDescriptionInit];
-
-export type RTCCandidateEvent = ["ice-candidate", RTCIceCandidateInit];
-
-export type RTCEvent = RTCOfferEvent | RTCAnswerEvent | RTCCandidateEvent;
-export type RTCEventType = RTCEvent[0];
-export type RTCEventPayload<T extends RTCEventType> = Extract<
-  RTCEvent,
-  [T, any]
->[1];
-
 export type RTCEventMap = {
   offer: (payload: RTCSessionDescriptionInit) => void;
   answer: (payload: RTCSessionDescriptionInit) => void;
   "ice-candidate": (payload: RTCIceCandidateInit) => void;
 };
+
+export type RTCEventPayload<T extends keyof RTCEventMap> = Parameters<
+  RTCEventMap[T]
+>[0];
