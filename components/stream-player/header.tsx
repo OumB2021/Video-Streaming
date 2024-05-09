a21daa7aca23732216686a6c627369c558958a4f
Oumb2021 <baarryoumar@gmail.com>
2024-05-08 23:18:26 -0400

    video description badge

--- components/stream-player/header.tsx
+++ components/stream-player/header.tsx
@@ -1,11 +1,68 @@
 "use client";
 
-interface HeaderProps {}
+import {
+  useParticipants,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+import { UserAvatar } from "../user-avatar";
+import { VerifiedMark } from "../verified-mark";
+import { UserIcon } from "lucide-react";
 
-export const Header = ({}: HeaderProps) => {
+interface HeaderProps {
+  hostName: string;
+  hostIdentity: string;
+  viewerIdentity: string;
+  imageUrl: string;
+  isFollowing: boolean;
+  name: string;
+}
+
+export const Header = ({
+  hostName,
+  hostIdentity,
+  viewerIdentity,
+  imageUrl,
+  isFollowing,
+  name,
+}: HeaderProps) => {
+  const participants = useParticipants();
+  const participant = useRemoteParticipant(hostIdentity);
+  const isLive = !!participant;
+  const participantCount = participants.length - 1;
+  const hostAsViewer = `host-${hostIdentity}`;
+  const isHost = viewerIdentity === hostAsViewer;
+
   return (
-    <div>
-      <p>This is the header</p>
+    <div className="flex flex-col lg:flex-row gap-y-4 lg:gap-y-0 items-start justify-between px-4">
+      <div className="flex items-center gap-x-3">
+        <UserAvatar
+          imageUrl={imageUrl}
+          username={hostName}
+          size="lg"
+          isLive={isLive}
+          showBadge={true}
+        />
+        <div className="space-y-1">
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-lg font-semibold">{hostName}</h2>
+            <VerifiedMark />
+          </div>
+          <p className="text-sm ">{name}</p>
+          {isLive ? (
+            <div className="font-semibold flex gap-x-1 items-center text-xs text-rose-500">
+              <UserIcon className="h-4 w-4" />
+              <p>
+                {participantCount}{" "}
+                {participantCount === 1 ? "viewer" : "viewers"}
+              </p>
+            </div>
+          ) : (
+            <p className="font-semibold text-xs text-muted-foreground">
+              Offline
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
