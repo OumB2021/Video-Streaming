77b7b646fd7e7b1776e35348d97d3e56729ac29a
Oumb2021 <baarryoumar@gmail.com>
2024-05-10 05:40:36 -0400

    home page toggle

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,13 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import { Results, ResultsSkeleton } from "./_components/results";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Button } from "@/components/ui/button";
+import { getStreams } from "@/lib/feed-service";
 
 export default function Home() {
+  const [activeToggle, setActiveToggle] = useState("LIVEKIT");
+  const handleToggle = (value) => {
+    setActiveToggle(value);
+  };
+
   return (
-    <div className="h-full p-8 max-w-screen-2xl mx-auto">
-      <Suspense fallback={<ResultsSkeleton />}>
-        <Results />
-      </Suspense>
-    </div>
+    <>
+      <ToggleGroup className="m-4" type="single">
+        <ToggleGroupItem
+          value="LIVEKIT"
+          aria-label="LIVEKIT"
+          onClick={() => handleToggle("LIVEKIT")}
+        >
+          <div className="font-semibold">LIVEKIT</div>
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          value="WEBRTC"
+          aria-label="WEBRTC"
+          onClick={() => handleToggle("WEBRTC")}
+        >
+          <div>WEBRTC</div>
+        </ToggleGroupItem>
+      </ToggleGroup>
+      <div className="h-full p-8 max-w-screen-2xl mx-auto">
+        {activeToggle === "LIVEKIT" && (
+          <Suspense fallback={<ResultsSkeleton />}>
+            <Results />
+          </Suspense>
+        )}
+        {activeToggle === "WEBRTC" && <div>WEBRTC</div>}
+      </div>
+    </>
   );
 }
